89d7272871104f952b6952db3d20cd0afc38c186
Hunter Dorminey <hdormine@students.kennesaw.edu>
2023-03-16 13:45:07 -0400

    Main UI elements for Welcome screen finished.

--- App.js
+++ App.js
@@ -1,74 +1,9 @@
-import {StatusBar} from 'expo-status-bar';
-import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import SelectDropdown from 'react-native-select-dropdown'
-
-const emojis = ['Happy', 'Sad', 'Mad'];
+import welcomeScreen from "./screens/welcomeScreen";
 
 export default function App() {
     return (
-        <View style={styles.frontPage}>
-            <Text style={styles.titleText}>Welcome</Text>
-            <Text style={styles.selectText}>Select an emoji</Text>
-            <StatusBar style="auto"/>
-            <SelectDropdown
-                data={emojis}
-                defaultButtonText={' '}
-                buttonStyle={styles.dropdownButton}
-                onSelect={(selectedItem) => {
-                    console.log(selectedItem);
-                }}
-                buttonTextAfterSelection={(selectedItem) => {
-                    return selectedItem;
-                }}
-                rowTextForSelection={(item) => {
-                    return item;
-                }}
-            />
-            <TouchableOpacity
-                style={styles.continueButton}
-                onPress={() => Alert.alert('Continue button pressed')}
-                underlayColor='white'>
-                <Text style={styles.continueButtonText}>Continue</Text>
-            </TouchableOpacity>
-        </View>
-    );
+        welcomeScreen()
+    )
 }
 
-const styles = StyleSheet.create({
-    frontPage: {
-        flex: 1,
-        backgroundColor: '#003268',
-        alignItems: 'center',
-    },
-    titleText: {
-        fontSize: 65,
-        fontWeight: "normal",
-        color: 'white',
-        top: '25%'
-    },
-    selectText: {
-        fontSize: 25,
-        fontWeight: "300",
-        color: 'white',
-        top: '30%'
-    },
-    dropdownButton: {
-        width: 100,
-        height: 30,
-        borderRadius: 15,
-        top: '68%'
-    },
-    continueButton: {
-        backgroundColor: '#c62f4e',
-        borderRadius: '30',
-        width: 250,
-        height: 60,
-        justifyContent: 'center',
-        top: '60%'
-    },
-    continueButtonText: {
-        fontSize: 35,
-        color: 'white',
-        textAlign: 'center'
-    }
-});
+
